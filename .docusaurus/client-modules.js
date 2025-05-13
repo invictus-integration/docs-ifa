62bdb9fb8746c5c108ce4112b3550c4cc5bf5cc4
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-05-13 08:23:24 +0200

    docs(docusaurus): delete local .docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\dev\\github.com\\invictus-docs\\node_modules\\.pnpm\\infima@0.2.0-alpha.43\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\dev\\github.com\\invictus-docs\\node_modules\\.pnpm\\@docusaurus+theme-classic@3_3ee9aa7748d97313e6c6dba6dd489707\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\dev\\github.com\\invictus-docs\\node_modules\\.pnpm\\@docusaurus+theme-classic@3_3ee9aa7748d97313e6c6dba6dd489707\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\dev\\github.com\\invictus-docs\\src\\css\\custom.css"),
-];
