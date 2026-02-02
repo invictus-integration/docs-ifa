40c952c68724ee1f812dbedf567ba76ba0c77f69
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2026-02-02 08:11:52 +0100

    chore(gh): use sitemap.xml

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,25 @@
+import fs from 'fs';
+import convert from 'xml-js';
+
+const SITEMAP_PATH = './build/sitemap.xml';
+const SITE_URL = 'https://invictus-integration.github.io'
+
+const data = fs.readFileSync(SITEMAP_PATH);
+const result = convert.xml2json(data, { compact: true, spaces: 4 });
+const json = JSON.parse(result);
+const url = json.urlset.url.map((item) => item.loc._text.replace(SITE_URL, ""));
+
 module.exports = {
-  collect: {
-    staticDirFileDiscoveryDepth: 10
-  },
   ci: {
+    collect: {
+      staticDirFileDiscoveryDepth: 10,
+      url: url
+    },
+    assert: {
+      preset: 'lighthouse:recommended'
+    },
     upload: {
       target: 'temporary-public-storage',
     },
-
   },
 };
