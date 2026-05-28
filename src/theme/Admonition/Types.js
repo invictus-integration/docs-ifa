35b353c6e094ab73e37e81ae1811cdb98ff9af8a
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2026-05-28 10:32:01 +0200

    chore(release-notes): use same color palette for release notes coloring (#573)
    Use the same coloring scheme for the release notes page, while focusing
    on text instead of relying on built-in Docusaurus admonitions colors.

--- src/theme/Admonition/Types.js
+++ src/theme/Admonition/Types.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
 import PraiseAdmonition from './Type/praise';
-import FeatureAdmonition from './Type/feature';
-import TechAdmonition from './Type/tech';
-import FixAdmonition from './Type/fix';
 
 const AdmonitionTypes = {
   ...DefaultAdmonitionTypes,
-  'praise': PraiseAdmonition,
-  'feature': FeatureAdmonition,
-  'tech': TechAdmonition,
-  'fix': FixAdmonition,
+  'praise': PraiseAdmonition
 };
 
 export default AdmonitionTypes;
