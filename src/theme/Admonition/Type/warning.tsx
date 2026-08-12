8bf76c4212bfa6ea71f122b88e1a6b13682c70d8
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2026-08-12 07:04:33 +0200

    chore(admonition): extract common admonition theme style (#668)
    The Docusaurus swizzled admonition types duplicated their theme,
    resulting in a high technical debt cost if icons change. This PR
    extracts this to a common/shared theme for all (and future) admonitions
    to use.

--- src/theme/Admonition/Type/warning.tsx
+++ src/theme/Admonition/Type/warning.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import clsx from 'clsx';
 import Translate from '@docusaurus/Translate';
-import type { Props } from '@theme/Admonition/Type/Warning';
-import AdmonitionLayout from '@theme/Admonition/Layout';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import createAdmonitionType from './createAdmonitionType';
 import styles from './warning.module.css';
 
-const infimaClassName = 'alert alert--warning';
-
-const defaultProps = {
+export default createAdmonitionType({
+  infimaClassName: 'alert alert--warning',
   icon: <FontAwesomeIcon icon={faTriangleExclamation} />,
   title: (
     <Translate
@@ -18,15 +14,5 @@ const defaultProps = {
       warning
     </Translate>
   ),
-};
-
-export default function AdmonitionTypeWarning(props: Props): JSX.Element {
-  return (
-    <AdmonitionLayout
-      {...defaultProps}
-      {...props}
-      className={clsx(infimaClassName, styles.admonition, props.className)}>
-      {props.children}
-    </AdmonitionLayout>
-  );
-}
+  admonitionClassName: styles.admonition,
+});
